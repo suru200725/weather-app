1691a863188a144c3249dda2599bc30092b1bc7b
suru200725 <shrutipandey2024v@gmail.com>
2026-04-10 02:57:57 +0530

    weatherapp

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
 
-API_KEY = os.getenv("e857561711d2b3c177b628f4dee0465b")
+API_KEY = "e857561711d2b3c177b628f4dee0465b"
 BASE_URL = "https://api.openweathermap.org/data/2.5"
+
+#hiii
--- utils/weather.py
+++ utils/weather.py
@@ -1,20 +1,58 @@
 import requests
 from config import API_KEY, BASE_URL
+from datetime import datetime
+
 
 def get_weather(city):
 
-    url = f"{BASE_URL}?q={city}&appid={API_KEY}&units=metric"
+    url = f"{BASE_URL}/weather?q={city}&appid={API_KEY}&units=metric"
+
     response = requests.get(url)
 
     if response.status_code == 200:
         data = response.json()
+
+        # 👉 PUT YOUR WEATHER DICTIONARY HERE
         weather = {
             "city": data["name"],
             "temperature": data["main"]["temp"],
             "description": data["weather"][0]["description"],
+            "humidity": data["main"]["humidity"],
+            "wind": data["wind"]["speed"],
             "icon": data["weather"][0]["icon"]
         }
+
         return weather
+
+def get_forecast(city):
+
+    url = f"{BASE_URL}/forecast?q={city}&appid={API_KEY}&units=metric"
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        forecast_data = []
+
+        for i in range(0, 40, 8):  # 5 days
+            item = data["list"][i]
+
+            date_obj = datetime.strptime(item["dt_txt"], "%Y-%m-%d %H:%M:%S")
+
+            forecast_data.append({
+                "date": date_obj.strftime("%A"),
+                "temp": item["main"]["temp"],
+                "description": item["weather"][0]["description"],
+                "icon": item["weather"][0]["icon"]
+            })
+
+        return forecast_data
+
     else:
-        print("Error fetching weather data")
+        print("Error fetching forecast")
         return None
+    
+    # else:
+    #     print("Error fetching weather data")
+    #     return None
